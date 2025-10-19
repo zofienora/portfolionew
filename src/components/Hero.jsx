9e447e8fa2ba0a422e59bb2962e174d86096dfdc
Zofia Nora <sophienora92@gmail.com>
2025-10-19 11:35:30 +0200

    added first heading at hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
 function Hero() {
     return (
         <>
+            <div className="container">
+                <h1 className="hero-h-one">Sophie Nora Keil</h1>
+                <h2 className="hero-h-two">Frontend Developer and UX/UI Designer</h2>
+            </div>
         </>
     )
 }
