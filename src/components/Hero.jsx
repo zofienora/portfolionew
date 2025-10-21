acb3d39858ba436723ac806d34b1af884c03dfe2
Zofia Nora <sophienora92@gmail.com>
2025-10-21 11:16:16 +0200

    added all photos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,13 @@ import * as motion from "motion/react-client"
 import { useRef } from "react";
 import beanie from "../assets/img/beanie.png"
 import certificate from "../assets/img/certificate.png"
+import controller from "../assets/img/controller.png"
 import fire from "../assets/img/fire.jpg"
 import ice from "../assets/img/ice.png" 
 import mac from "../assets/img/mac.png"
 import mario from "../assets/img/mario.png"
 import shoe from "../assets/img/shoe.png"
-import snow from "../assets/img/snow.png"
+import snow from "../assets/img/snow.jpg"
 import swim from "../assets/img/swim.png"
 import toast from "../assets/img/toast.png"
 
@@ -41,6 +42,62 @@ function Hero() {
                     dragConstraints={constraintsRef}
                     dragElastic={0.2}
                     style={picthree}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picfour}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picfive}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picsix}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picseven}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={piceight}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picnine}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={picten}
+                />
+
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    style={piceleven}
                 />
                
                
@@ -94,7 +151,111 @@ const pictwo = {
 const picthree = {
   width: 300,
   height: 220,
+  backgroundImage: `url(${controller})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picfour = {
+  width: 300,
+  height: 220,
   backgroundImage: `url(${fire})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picfive = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${ice})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picsix = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${mac})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picseven = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${mario})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const piceight = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${shoe})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picnine = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${snow})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const picten = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${swim})`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  position: 'absolute',
+  top: '50%',
+  left: '20%',
+  backgroundPosition: 'center',
+  borderRadius: 10,
+};
+
+const piceleven = {
+  width: 300,
+  height: 220,
+  backgroundImage: `url(${toast})`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   position: 'absolute',
