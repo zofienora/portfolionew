8726560ffd3f353772de24670a42c642b6755326
Zofia Nora <sophienora92@gmail.com>
2025-10-23 17:01:32 +0200

    added about me motion draggable div

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,32 @@
+import * as motion from "motion/react-client"
+import { useRef } from "react";
+
 function About() {
+    const constraintsRef = useRef(null)
+
     return (
         <>
+            <motion.div className="drag-container" ref={constraintsRef} style={constraints}>
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragElastic={0.2}
+                    />
+                    <h2 className="about-headline">about</h2>
+                    <p className="about-text"></p>
+            </motion.div>
+        
+
         </>
     )
 }
 
 export default About;
+
+const constraints = {
+  width: '100vw',   // full viewport width
+  height: '100vh',  // full viewport height
+  backgroundColor: 'var(--hue-1-transparent)',
+  overflow: 'hidden',       // optional: hides overflow
+  position: 'relative'      // important for drag constraints
+};
